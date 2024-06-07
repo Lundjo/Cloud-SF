f4dc1349446df473156816fc724ab6fc99437623
Lundjo <lukadjelic529@gmail.com>
2024-06-07 16:39:06 +0200

    zavrsen post controller

--- backend/API/Controllers/PostController.cs
+++ backend/API/Controllers/PostController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.Models.post;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 using RedditDataRepository.blobs.images;
+using RedditDataRepository.classes.Comments;
 using RedditDataRepository.classes.Posts;
 using RedditDataRepository.cloud.account;
 using RedditDataRepository.posts.Create;
 using System.Net;
+using RedditDataRepository.comments.Read;
+using RedditDataRepository.posts.Read;
+using RedditDataRepository.auth.guard;
+using RedditDataRepository.comments.Delete;
+using RedditDataRepository.posts.Delete;
+using System.Web;
+using RedditDataRepository.posts.Update;
+using RedditDataRepository.users.Read;
 
 namespace API.Controllers
 {
@@ -109,6 +119,285 @@ namespace API.Controllers
                 return StatusCode((int)HttpStatusCode.InternalServerError);
             }
         }
+        #endregion
+
+        #region GET
+        /// <summary>
+        /// Get a specific post by post id
+        /// </summary>
+        /// <returns>
+        /// An <see cref="IHttpActionResult"/> representing the result of the operation with post object.
+        /// </returns>
+        [HttpGet("{postId}")]
+        public async Task<IActionResult> Get(string postId)
+        {
+            try
+            {
+                if (postId == null || postId == "")
+                {
+                    return BadRequest();
+                }
+
+                List<Comment> comments = await ReadComments.Execute(AzureTableStorageCloudAccount.GetCloudTable("comments"), postId);
+                Post post = await ReadPost.Run(AzureTableStorageCloudAccount.GetCloudTable("posts"), "Post", postId);
+
+                if (post == null)
+                {
+                    return NotFound();
+                }
+
+                // Create DTO object of post with comments
+                GetPost getPost = new GetPost(post.Id, post.Author, post.Title, post.Content, post.HasImage, post.ImageBlobUrl, comments.OrderByDescending(x => x.Timestamp).ToList());
+
+                return Ok(getPost);
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+        #endregion
+
+        #region DELETE
+        /// <summary>
+        /// Deletes a post with the specified post ID.
+        /// </summary>
+        /// <param name="postId">The ID of the post to delete.</param>
+        /// <returns>
+        /// IHttpActionResult representing the result of the deletion operation.
+        /// Returns Ok() if the post and its associated comments were successfully deleted.
+        /// Returns Unauthorized() if the request is not authorized.
+        /// Returns BadRequest() if the comments associated with the post could not be deleted.
+        /// Returns NotFound() if the post to delete is not found.
+        /// Returns InternalServerError() if an unexpected error occurs during the deletion process.
+        /// </returns>
+        [HttpDelete("{postId}")]
+        [JwtAuthenticationFilter] // Requires JWT authentication
+        public async Task<IActionResult> Delete(string postId)
+        {
+            try
+            {
+                // Retrieve the post author by ID
+                string author = await ReadPostAuthor.Execute(AzureTableStorageCloudAccount.GetCloudTable("posts"), postId);
+
+                // Only the author of the post can delete it
+                if (!ResourceGuard.RunCheck(HttpContext, author))
+                {
+                    // Return unauthorized if the request is not authorized
+                    return Unauthorized();
+                }
+
+                // Delete the post
+                bool deleteResult = await DeletePost.Execute(AzureTableStorageCloudAccount.GetCloudTable("posts"), postId);
+
+                if (deleteResult)
+                {
+                    // Delete all comments associated with the post
+                    bool deleteCommentsResult = await RemoveComments.Execute(AzureTableStorageCloudAccount.GetCloudTable("comments"), postId);
+
+                    if (deleteCommentsResult)
+                    {
+                        return Ok();
+                    }
+                    else
+                    {
+                        // Return BadRequest if comments associated with the post could not be deleted
+                        return BadRequest();
+                    }
+                }
+                else
+                {
+                    // Return NotFound if the post to delete is not found
+                    return NotFound();
+                }
+            }
+            catch (Exception e)
+            {
+                // Return InternalServerError if an unexpected error occurs during the deletion process
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+        #endregion
+
+        #region GET POSTS
+
+        /// <summary>
+        /// Retrieves posts based on pagination parameters such as post ID, search keywords, sorting criteria, and time.
+        /// </summary>
+        /// <param name="postId">The ID of the post to start pagination from.</param>
+        /// <param name="searchKeywords">Keywords to filter posts by.</param>
+        /// <param name="sort">The sorting criterion for the posts.</param>
+        /// <param name="time">The timestamp indicating the starting point for pagination.</param>
+        /// <returns>
+        /// An IHttpActionResult containing a list of posts retrieved based on pagination parameters if successful;
+        /// otherwise, an InternalServerError result containing the encountered exception.
+        /// </returns>
+        [HttpGet("{postId}/{searchKeywords}/pagination/{sort}/{time}")]
+        public async Task<IActionResult> Pagination(string postId, string searchKeywords, int sort, string time)
+        {
+            try
+            {
+                // Convert the provided timestamp string to a DateTimeOffset object
+                DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(time));
+                DateTime newtime = dateTimeOffset.UtcDateTime;
+
+                // Initialize remaining number of posts to be retrieved and a list to store retrieved posts
+                int remaining = 3;
+                List<Post> posts = new List<Post>();
+
+                // Perform pagination until remaining posts to retrieve is zero
+                while (remaining > 0)
+                {
+                    // Retrieve posts based on pagination parameters
+                    var currentPosts = await ReadPosts.Execute(AzureTableStorageCloudAccount.GetCloudTable("posts"), postId, remaining, searchKeywords, sort, newtime);
+
+                    // If no more posts are retrieved, break out of the pagination loop
+                    if (currentPosts.Count == 0)
+                    {
+                        break;
+                    }
+                    else if (posts.Count <= remaining)
+                    {
+                        return Ok(currentPosts);
+                    }
+
+                    // Add retrieved posts to the list and update remaining count
+                    posts.AddRange(currentPosts);
+                    remaining -= currentPosts.Count();
+                }
+
+                // Return an OK result containing the list of retrieved posts
+                return Ok(posts);
+            }
+            catch (Exception e)
+            {
+                // Return an InternalServerError result containing the encountered exception if an error occurs
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+
+        #endregion
+
+        #region GET USERS POSTS
+
+
+        /// <summary>
+        /// Retrieves posts by a single user based on pagination parameters such as post ID, search keywords, sorting criteria, and time.
+        /// </summary>
+        /// <param name="postId">The ID of the post to start pagination from.</param>
+        /// <param name="searchKeywords">Keywords to filter posts by.</param>
+        /// <param name="sort">The sorting criterion for the posts.</param>
+        /// <param name="time">The timestamp indicating the starting point for pagination.</param>
+        /// <param name="encodedEmail">Email address of user whose posts to retrieve.</param>
+        /// <returns>
+        /// An IHttpActionResult containing a list of posts retrieved based on pagination parameters if successful;
+        /// otherwise, an InternalServerError result containing the encountered exception.
+        /// </returns>
+        [HttpGet("{postId}/{searchKeywords}/userPosts/{sort}/{time}/{encodedEmail}")]
+        [JwtAuthenticationFilter]
+        public async Task<IActionResult> UserPosts(string postId, string searchKeywords, int sort, string time, string encodedEmail)
+        {
+            try
+            {
+                // Convert the provided timestamp string to a DateTimeOffset object
+                DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(time));
+                DateTime newtime = dateTimeOffset.UtcDateTime;
+
+                // Initialize remaining number of posts to be retrieved and a list to store retrieved posts
+                int remaining = 3;
+                List<Post> posts = new List<Post>();
+
+                // Perform pagination until remaining posts to retrieve is zero
+                while (remaining > 0)
+                {
+                    // Retrieve posts based on pagination parameters and user
+                    var currentPosts = await ReadUsersPosts.Execute(AzureTableStorageCloudAccount.GetCloudTable("posts"), postId, remaining, searchKeywords, sort, newtime, HttpUtility.UrlDecode(encodedEmail));
+
+                    // If no more posts are retrieved, break out of the pagination loop
+                    if (currentPosts.Count == 0)
+                    {
+                        break;
+                    }
+                    else if (posts.Count <= remaining)
+                    {
+                        return Ok(currentPosts);
+                    }
+
+                    // Add retrieved posts to the list and update remaining count
+                    posts.AddRange(currentPosts);
+                    remaining -= currentPosts.Count();
+                }
+
+                // Return an OK result containing the list of retrieved posts
+                return Ok(posts);
+            }
+            catch (Exception e)
+            {
+                // Return an InternalServerError result containing the encountered exception if an error occurs
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+
+        #endregion
+
+        #region SUBSCRIBE
+        /// <summary>
+        /// Subscribes a user to a post
+        /// </summary>
+        /// <param name="postId">The ID of the post</param>
+        /// <param name="encodedEmail">Email of the user who wants to subscribe</param>
+        /// <returns>
+        /// IHttpActionResult representing the result of the insert subscription operation.
+        /// Returns Ok() if post exists, user exists, and if operation is completed successfully.
+        /// Returns Unauthorized() if the email from the token and email in the request don't match.
+        /// Returns BadRequest() if the user with associated email is not found.
+        /// Returns NotFound() if the post doesn't exist.
+        /// Returns InternalServerError() if an unexpected error occurs during the subscription process.
+        /// </returns>
+        [HttpGet("{postId}/{encodedEmail}/subscribe")]
+        [JwtAuthenticationFilter]
+        public async Task<IActionResult> Subscribe(string postId, string encodedEmail)
+        {
+            try
+            {
+                // Check if post exists by checking for its author
+                string author = await ReadPostAuthor.Execute(AzureTableStorageCloudAccount.GetCloudTable("posts"), postId);
+                if (author == null)
+                {
+                    return NotFound();
+                }
+                // Check if user exists
+                string email = HttpUtility.UrlDecode(encodedEmail);
+                if (!(await IsUserExists.RunCheckAsync(AzureTableStorageCloudAccount.GetCloudTable("users"), email)))
+                {
+                    return BadRequest();
+                }
+                // Checking if user is who he represents he is
+                if (!ResourceGuard.RunCheck(HttpContext, email))
+                {
+                    return Unauthorized();
+                }
+                // Add check is subscription already exists
+                var already_subsrcibed = await ReadSubscriptions.Run(AzureTableStorageCloudAccount.GetCloudTable("subscriptions"), postId);
+
+                // If it is already subkrajzed return none
+                if (already_subsrcibed.FirstOrDefault(p => p == email) != null)
+                    return BadRequest();
+
+                // Adding subscription 
+                var insertResult = await SubscribeToPost.Execute(AzureTableStorageCloudAccount.GetCloudTable("subscriptions"), postId, email);
+                if (insertResult)
+                {
+                    return Ok();
+                }
+                return NotFound();
+            }
+            catch (Exception e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+
         #endregion
     }
 }
